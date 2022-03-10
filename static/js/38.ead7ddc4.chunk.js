c80c3006489c21cb53e821c1e85810ee7570cb9f
lhd-dfactory <83153425+lhd-defactory@users.noreply.github.com>
2022-03-10 10:11:54 +0800

    release devnet

---
+++ static/js/38.ead7ddc4.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpderi-client"]=this["webpackJsonpderi-client"]||[]).push([[38],{1736:function(n,i){},2083:function(n,i){},2090:function(n,i){}}]);
+//# sourceMappingURL=38.ead7ddc4.chunk.js.map
---
+++ static/js/39.fbcdb621.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpderi-client"]=this["webpackJsonpderi-client"]||[]).push([[39],{1736:function(n,i){},2299:function(n,i){},2368:function(n,i){}}]);
+//# sourceMappingURL=39.fbcdb621.chunk.js.map
