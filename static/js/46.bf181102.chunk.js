90aa96924b9eac3ed872ba05e1e24fee42644258
huangwenchao2000 <76576293+huangwenchao2000@users.noreply.github.com>
2022-05-10 12:40:39 +0800

    release devnet

---
+++ static/js/46.bf181102.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpderi-client"]=this["webpackJsonpderi-client"]||[]).push([[46],{2045:function(i,e){}}]);
+//# sourceMappingURL=46.bf181102.chunk.js.map
---
+++ static/js/47.de107972.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpderi-client"]=this["webpackJsonpderi-client"]||[]).push([[47],{2190:function(i,e){}}]);
+//# sourceMappingURL=47.de107972.chunk.js.map
