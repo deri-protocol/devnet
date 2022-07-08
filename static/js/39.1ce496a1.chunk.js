be069c675aab0ee0e33d310df2fc9d90e78556a4
huangwenchao2000 <>
2022-07-08 18:00:06 +0800

    release devnet

---
+++ static/js/39.1ce496a1.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpderi-client"]=this["webpackJsonpderi-client"]||[]).push([[39],{1902:function(n,i){},2236:function(n,i){},2243:function(n,i){}}]);
+//# sourceMappingURL=39.1ce496a1.chunk.js.map
---
+++ static/js/40.414c7c34.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpderi-client"]=this["webpackJsonpderi-client"]||[]).push([[40],{1902:function(n,i){},2436:function(n,i){},2505:function(n,i){}}]);
+//# sourceMappingURL=40.414c7c34.chunk.js.map
